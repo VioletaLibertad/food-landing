a7fbc2cf92ffc21e175ab9c8938d862683b85a8c
Violeta Libertad <morada.libertad@gmail.com>
2020-06-21 21:44:07 -0400

    temporary solution to sticky socials

--- src/views/Home/Header/index.js
+++ src/views/Home/Header/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import logo from '../../../assets/img/logo.svg';
 import headerBrush from '../../../assets/img/hero-shape.png';
 import './header.css';
+import Socials from '../../../components/Socials';
 
 function Header() {
   return (
     <header className="bg_img">
-        <div className="header_nav">
-          <img src={logo} className="header_logo" alt="header_logo"/>
-        </div>
-        <div className="header_title">
-          <h2>El secreto de tu cocina</h2>
-        </div>
-        <div className="header_title_decoration">
-          <img src={headerBrush} alt="header_decoration"/>
-        </div>
+      <div className="header_nav">
+        <img src={logo} className="header_logo" alt="header_logo"/>
+        <Socials />
+      </div>
+      <div className="header_title">
+        <h2>El secreto de tu cocina</h2>
+      </div>
+      <div className="header_title_decoration">
+        <img src={headerBrush} alt="header_decoration"/>
+      </div>
     </header>
   );
 }
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from './Header';
 import Articles from './Articles/index';
-import Socials from '../../components/Socials/index';
 import './styles.css';
 
 function Home() {
   return (
     <>
-      <Socials />
       <Header />
       <Articles />
     </>
