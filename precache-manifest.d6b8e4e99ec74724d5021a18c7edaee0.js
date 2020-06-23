c16f926111d68b3fd264170b179d86fbb4deb708
Violeta Libertad <morada.libertad@gmail.com>
2020-06-22 21:16:10 -0400

    Updates

---
+++ precache-manifest.d6b8e4e99ec74724d5021a18c7edaee0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2606497ad18b1a78bd00cff83ea1595",
+    "url": "/food-landing/index.html"
+  },
+  {
+    "revision": "e0194553de8e46d0c453",
+    "url": "/food-landing/static/css/main.afb7c6e6.chunk.css"
+  },
+  {
+    "revision": "d7e887db386e545b3411",
+    "url": "/food-landing/static/js/2.6538ab3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-landing/static/js/2.6538ab3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0194553de8e46d0c453",
+    "url": "/food-landing/static/js/main.8b9d2090.chunk.js"
+  },
+  {
+    "revision": "bdb46ab2ecd165d74484",
+    "url": "/food-landing/static/js/runtime-main.dff22af7.js"
+  },
+  {
+    "revision": "137184dba54b51a694902553fdf2f3e2",
+    "url": "/food-landing/static/media/facebook-icon.137184db.svg"
+  },
+  {
+    "revision": "286d6d231df58ff1889535e68fa45d45",
+    "url": "/food-landing/static/media/heading-shape.286d6d23.png"
+  },
+  {
+    "revision": "38c977ebbcd1c29b110df99bf6184f99",
+    "url": "/food-landing/static/media/hero-background.38c977eb.jpg"
+  },
+  {
+    "revision": "bf37f5f129581f29f44345841c0e740b",
+    "url": "/food-landing/static/media/hero-shape.bf37f5f1.png"
+  },
+  {
+    "revision": "42609a9901f0368a9ce44cdd3fe00651",
+    "url": "/food-landing/static/media/instagram-icon.42609a99.svg"
+  },
+  {
+    "revision": "d0836d9e559de5dfbea72fd53c856d4a",
+    "url": "/food-landing/static/media/logo.d0836d9e.svg"
+  },
+  {
+    "revision": "1329efff629e7a197cec727238538b80",
+    "url": "/food-landing/static/media/youtube-icon.1329efff.svg"
+  }
+]);
